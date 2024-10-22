f83abb0215486c7e79c91f1fe4b3ff658a9e341f
Maciej Szostak <maciek@reco.studio>
2024-10-22 11:08:03 +0200

    Initial commit

---
+++ MilleShopingApp/MilleShopingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilleShopingAppApp.swift
+//  MilleShopingApp
+//
+//  Created by Maciej Szostak on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MilleShopingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
